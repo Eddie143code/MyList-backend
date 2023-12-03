b39eed9b337f42748b609bbd8e9fdd92a74c2c5f
Ettiene le Roux <ettienejoubertleroux@gmail.com>
2023-12-03 19:04:32 +0200

    user model

---
+++ Model/MyListContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyList_backend.Model
+{
+    public class MyListContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+
+        public MyListContext(DbContextOptions options) : base(options) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using MyList_backend.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MyListContext>(options =>
+{
+    options.UseSqlServer("");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
